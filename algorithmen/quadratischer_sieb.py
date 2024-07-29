5038a98f3b831c74ac0efd10ab2289025a925bf9
ts19084 <thomas.schauer-koeckeis@stud.th-deg.de>
2024-07-30 01:44:01 +0200

    Rewritten everything with numpy and added fast gaussian elimination

--- algorithmen/quadratischer_sieb.py
+++ algorithmen/quadratischer_sieb.py
@@ -2,14 +2,12 @@ import random
 import typing
 
 import numpy as np
-import scipy.linalg as sc
 
 
 def quad_residue(a, n):
     # checks if a is quad residue of n
-    l = 1
     q = (n - 1) // 2
-    x = q**l
+    x = q**1
     if x == 0:
         return 1
 
@@ -26,47 +24,108 @@ def quad_residue(a, n):
     return z
 
 
-def factorise(number: int, n: int, b: int):
-    # (N**1/2 + x) - N = y_not_works for x in range(1, n)
-    # y_not_works factorise this
-    # only use those whos are under the smoothness bound (b)
-    # calculate a perfect prime with multiplying different y_not_works's
-    # do it over a matrix, with only having all exponents % 2 -> y
-    # x**2 = y**2 % N
-    # gcd(x-y, N)
-    # n > b !!
-    primes = [
-        x
-        for x in range(2, b + 1)
-        if all(x % i != 0 for i in range(2, int(x**0.5) + 1))
-        and quad_residue(x, number)
-    ]
-    x = int(number ** (1 / 2)) + 1
-    smooth_primes = []
-    ts = []
-    for j in primes:
-        response, _ = tonnelli_shanks(number, j)
-        t1 = (-x - response) % j
-        t2 = (-x + response) % j
-        ts.append((t1, t2))
-    for i in range(n):
-        smooth_exponents = []
-        y = (x + i) ** 2 - number
-        for j, p in enumerate(primes):
-            if not any(i % p == k for k in ts[j]):
-                smooth_exponents.append(0)
-                continue
-            exponent = 0
-            while True:
-                if y % p != 0:
-                    break
-                y = y // p
-                exponent += 1
-            smooth_exponents.append(exponent % 2)
-        smooth_primes.append(smooth_exponents) if y == 1 else None
-    print(np.array(smooth_primes))
-    print(np.transpose(smooth_primes))
-    print(sc.null_space(np.transpose(smooth_primes)))
+class quadratic_sieve:
+    def __init__(self, number: int, n: int, b: int):
+        self.number = number
+        self.n = n
+        self.b = b
+        self.x = int(number ** (1 / 2)) + 1
+        self.primes, self.ts = self.get_primes(number, b, self.x)
+
+    def compute_tonelli(self, prime, x, number):
+        response, _ = tonnelli_shanks(number, int(prime))
+        t1 = (-x - response) % prime
+        t2 = (-x + response) % prime
+        print(t1, t2)
+        return t1, t2
+
+    def compute_smooth_exponents(
+        self, j: int, prime: int, i: int, x: int, number: int, y: int
+    ) -> int:
+        # print("Smooth_exponent: ", j, prime)
+        if not any(i % prime == k for k in self.ts[j]):
+            return 0
+        exponent = 0
+        while True:
+            if y % prime != 0:
+                break
+            y = y // prime
+            exponent += 1
+        return exponent
+
+    def compute_smooth_primes(self, i: int, y: int, x: int, number: int) -> np.array:
+        # print("Smooth_prime: ", y, i)
+        return np.vectorize(self.compute_smooth_exponents)(
+            np.arange(len(self.primes)), self.primes, i, x, number, y
+        )
+
+    def get_primes(self, number: int, b: int, x: int):
+        # Calculating relevant primes
+        numbers = np.arange(2, b + 1)
+        is_prime = np.ones(len(numbers), dtype=bool)
+        for i in range(2, int(b**0.5) + 1):
+            if is_prime[i - 2]:
+                is_prime[i * i - 2 : b + 1 : i] = False
+        quad_residues = np.vectorize(quad_residue)(numbers, number).astype(bool)
+        primes = numbers[is_prime & quad_residues]
+        # Calculating steps to skip
+        ts = np.vectorize(self.compute_tonelli)(primes, x, number)
+        return primes, np.vstack(ts).transpose()
+
+    def factorise(self):
+        # (N**1/2 + x) - N = y_not_works for x in range(1, n)
+        # y_not_works factorise this
+        # only use those whos are under the smoothness bound (b)
+        # calculate a perfect prime with multiplying different y_not_works's
+        # do it over a matrix, with only having all exponents % 2 -> y
+        # x**2 = y**2 % N
+        # gcd(x-y, N)
+        # n > b !!
+
+        # Calculating array for sieving
+        i_array = np.arange(self.n)
+        y_array = (self.x + i_array) ** 2 - self.number
+        smooth_primes = []
+        print("Start")
+        smooth_primes = []
+
+        for idx, y in enumerate(y_array):
+            factors = self.compute_smooth_primes(idx, y, self.x, self.number)
+            if np.prod(self.primes**factors) == y:
+                smooth_primes.append(factors)
+
+        fast_matrix = np.array(smooth_primes)
+        # print(fast_matrix)
+        print(self.fast_gaussian_elimination(fast_matrix))
+
+    def fast_gaussian_elimination(self, matrix: np.array) -> np.array:
+        matrix = np.mod(matrix, 2)
+        rows, columns = matrix.shape
+        row_is_marked = np.array([False] * rows)
+        pivots = [-1] * columns
+        for i in range(columns):
+            pivot = np.nonzero(matrix[:, i] == 1)[-1]
+            if len(pivot) != 0:
+                print(pivot)
+                pivots[i] = pivot[0]
+                row_is_marked[pivot[0]] = True
+                cond = matrix[pivot[0], :] == 1
+                cond[i] = False
+                matrix[:, cond] ^= matrix[:, [i]]
+                print(matrix)
+        print(matrix)
+        marked_rows = matrix[row_is_marked, :]
+        unmarked_rows = matrix[~row_is_marked, :]
+        dependent_rows = np.any(
+            marked_rows[:, np.newaxis, :] & unmarked_rows[np.newaxis, :, :], axis=-1
+        ).transpose()[0]
+        print("marked: ", marked_rows)
+        print("unmarked: ", unmarked_rows)
+        print("dependent: ", dependent_rows)
+        perfect_square = np.concatenate(
+            (unmarked_rows, (marked_rows[dependent_rows, :]))
+        )
+        return perfect_square
 
 
 # David
@@ -120,3 +179,30 @@ def tonnelli_shanks(rhs: int, p: int) -> typing.Optional[tuple[int, int]]:
             m = i
 
         return y, (-y) % p
+        """primes = [
+            x
+            for x in range(2, b + 1)
+            if all(x % i != 0 for i in range(2, int(x**0.5) + 1))
+            and quad_residue(x, number)
+        ]"""
+        """for j in primes:
+            response, _ = tonnelli_shanks(number, j)
+            t1 = (-x - response) % j
+            t2 = (-x + response) % j
+            ts.append((t1, t2))"""
+
+        """for i in range(n):
+            smooth_exponents = []
+            y = (x + i) ** 2 - number
+            for j, p in enumerate(primes):
+                if not any(i % p == k for k in ts[j]):
+                    smooth_exponents.append(0)
+                    continue
+                exponent = 0
+                while True:
+                    if y % p != 0:
+                        break
+                    y = y // p
+                    exponent += 1
+                smooth_exponents.append(exponent % 2)
+            smooth_primes.append(smooth_exponents) if y == 1 else None"""
